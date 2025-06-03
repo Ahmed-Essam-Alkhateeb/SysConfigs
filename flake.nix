054c99dd6fd5c4734b85a6e1fa02a32a4551444d
Ahmed-Essam-Alkhateeb <Ahmed.Essam.Alkhateeb@proton.me>
2025-06-03 13:57:43 +0300

    Attempting to add home.nix to .dotfiles

--- flake.nix
+++ flake.nix
@@ -3,18 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    zen-browser.url = "github:youwen5/zen-browser-flake";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
   {
 
     nixosConfigurations.ashforest-host = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
+      inherit system;
       modules = [
         ./configuration.nix
       ];
+    };
+
+    homeConfigurations.ashforest = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home.nix ];
     };
 
   };
